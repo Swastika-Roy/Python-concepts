3b094d578e5068d1d356d15415e61faa758467f4
Swastika Roy <swastikaroytitu@gmail.com>
2026-01-06 20:56:08 +0530

    python programming

---
+++ 1st.py
@@ -0,0 +1,11 @@
+#Empty list
+empty_list = []
+print(empty_list)
+
+#list with integers
+numbers=[1,2,3,4,5]
+print(numbers)
+
+#list with strings
+fruits = ["apple","banana","cherry"]
+print(fruits)
